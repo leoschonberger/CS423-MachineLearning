84d133c8d501b7f99cf1abbcb2e57a9486ee8eae
Leo Schonberger <42780753+leoschonberger@users.noreply.github.com>
2025-05-17 12:20:11 -0700

    Debug: Fix missing titanicTransformer

--- library.py
+++ library.py
@@ -867,6 +867,60 @@ def find_random_state(
 
     return rs_value, Var
 
+# Analysis Funciton (Chapter 10)
+def threshold_results(thresh_list, actuals, predicted):
+    result_df = pd.DataFrame(columns=['threshold', 'precision', 'recall', 'f1', 'accuracy', 'auc'])
+    for t in thresh_list:
+        yhat = [1 if v >=t else 0 for v in predicted]
+        #note: where TP=0, the Precision and Recall both become 0. And I am saying return 0 in that case.
+        precision = precision_score(actuals, yhat, zero_division=0)
+        recall = recall_score(actuals, yhat, zero_division=0)
+        f1 = f1_score(actuals, yhat)
+        accuracy = accuracy_score(actuals, yhat)
+        auc = roc_auc_score(actuals, predicted)
+        result_df.loc[len(result_df)] = {'threshold':t, 'precision':precision, 'recall':recall, 'f1':f1, 'auc': auc, 'accuracy':accuracy}
+
+    result_df = result_df.round(2)
+
+    #Next bit fancies up table for printing. See https://betterdatascience.com/style-pandas-dataframes/
+    #Note that fancy_df is not really a dataframe. More like a printable object.
+    headers = {
+    "selector": "th:not(.index_name)",
+    "props": "background-color: #800000; color: white; text-align: center"
+    }
+    properties = {"border": "1px solid black", "width": "65px", "text-align": "center"}
+
+    fancy_df = result_df.style.highlight_max(color = 'pink', axis = 0).format(precision=2).set_properties(**properties).set_table_styles([headers])
+    return (result_df, fancy_df)
+
+
+# Custom Pipelines
+titanic_transformer = Pipeline(steps=[
+    # From Chapter 7
+    ('map_gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('map_class', CustomMappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
+    ('target_joined', CustomTargetTransformer(col='Joined', smoothing=10)),
+    ('tukey_age', CustomTukeyTransformer(target_column='Age', fence='outer')),
+    ('tukey_fare', CustomTukeyTransformer(target_column='Fare', fence='outer')),
+    ('scale_age', CustomRobustTransformer(target_column='Age')),
+    ('scale_fare', CustomRobustTransformer(target_column='Fare')),
+    ('impute', CustomKNNTransformer(n_neighbors=5)),
+    ], verbose=True)
+
+
+customer_transformer = Pipeline(steps=[
+    # From Chapter 7
+    ('map_os', CustomMappingTransformer('OS', {'Android': 0, 'iOS': 1})),
+    ('target_isp', CustomTargetTransformer(col='ISP')),
+    ('map_level', CustomMappingTransformer('Experience Level', {'low': 0, 'medium': 1, 'high':2})),
+    ('map_gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('tukey_age', CustomTukeyTransformer('Age', 'inner')),  #from chapter 4
+    ('tukey_time spent', CustomTukeyTransformer('Time Spent', 'inner')),  #from chapter 4
+    ('scale_age', CustomRobustTransformer(target_column='Age')), #from 5
+    ('scale_time spent', CustomRobustTransformer(target_column='Time Spent')), #from 5
+    ('impute', CustomKNNTransformer(n_neighbors=5)),
+    ], verbose=True)
+
 # Setup Funcitons
 def dataset_setup(original_table, label_column_name:str, the_transformer, rs, ts=.2):
     """
@@ -898,58 +952,3 @@ def titanic_setup(titanic_table, transformer=titanic_transformer, rs=titanic_var
 
 def customer_setup(customer_table, transformer=customer_transformer, rs=customer_variance_based_split, ts=.2):
     return dataset_setup(customer_table, 'Rating', transformer, rs, ts)
-
-# Analysis Funciton (Chapter 10)
-def threshold_results(thresh_list, actuals, predicted):
-    result_df = pd.DataFrame(columns=['threshold', 'precision', 'recall', 'f1', 'accuracy', 'auc'])
-    for t in thresh_list:
-        yhat = [1 if v >=t else 0 for v in predicted]
-        #note: where TP=0, the Precision and Recall both become 0. And I am saying return 0 in that case.
-        precision = precision_score(actuals, yhat, zero_division=0)
-        recall = recall_score(actuals, yhat, zero_division=0)
-        f1 = f1_score(actuals, yhat)
-        accuracy = accuracy_score(actuals, yhat)
-        auc = roc_auc_score(actuals, predicted)
-        result_df.loc[len(result_df)] = {'threshold':t, 'precision':precision, 'recall':recall, 'f1':f1, 'auc': auc, 'accuracy':accuracy}
-
-    result_df = result_df.round(2)
-
-    #Next bit fancies up table for printing. See https://betterdatascience.com/style-pandas-dataframes/
-    #Note that fancy_df is not really a dataframe. More like a printable object.
-    headers = {
-    "selector": "th:not(.index_name)",
-    "props": "background-color: #800000; color: white; text-align: center"
-    }
-    properties = {"border": "1px solid black", "width": "65px", "text-align": "center"}
-
-    fancy_df = result_df.style.highlight_max(color = 'pink', axis = 0).format(precision=2).set_properties(**properties).set_table_styles([headers])
-    return (result_df, fancy_df)
-
-
-
-# Custom Pipelines
-titanic_transformer = Pipeline(steps=[
-    # From Chapter 7
-    ('map_gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1})),
-    ('map_class', CustomMappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
-    ('target_joined', CustomTargetTransformer(col='Joined', smoothing=10)),
-    ('tukey_age', CustomTukeyTransformer(target_column='Age', fence='outer')),
-    ('tukey_fare', CustomTukeyTransformer(target_column='Fare', fence='outer')),
-    ('scale_age', CustomRobustTransformer(target_column='Age')),
-    ('scale_fare', CustomRobustTransformer(target_column='Fare')),
-    ('impute', CustomKNNTransformer(n_neighbors=5)),
-    ], verbose=True)
-
-
-customer_transformer = Pipeline(steps=[
-    # From Chapter 7
-    ('map_os', CustomMappingTransformer('OS', {'Android': 0, 'iOS': 1})),
-    ('target_isp', CustomTargetTransformer(col='ISP')),
-    ('map_level', CustomMappingTransformer('Experience Level', {'low': 0, 'medium': 1, 'high':2})),
-    ('map_gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1})),
-    ('tukey_age', CustomTukeyTransformer('Age', 'inner')),  #from chapter 4
-    ('tukey_time spent', CustomTukeyTransformer('Time Spent', 'inner')),  #from chapter 4
-    ('scale_age', CustomRobustTransformer(target_column='Age')), #from 5
-    ('scale_time spent', CustomRobustTransformer(target_column='Time Spent')), #from 5
-    ('impute', CustomKNNTransformer(n_neighbors=5)),
-    ], verbose=True)
